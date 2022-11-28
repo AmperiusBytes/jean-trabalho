cbdc0fc3f68d54235d065f8a748cbc8aa80694d3
Katriel Camilo <47159773+AmperiusBytes@users.noreply.github.com>
2022-11-28 11:21:13 -0300

    Add files via upload

---
+++ components/card.php
@@ -0,0 +1,12 @@
+<div>
+    <div class="d-flex">
+        <img src="" alt="" class="card-image">
+        <div class="card-info">
+            <h3>NOME</h3>
+            <div class="d-flex">
+                <p>ALTURA</p>
+                <p>IDADE</p>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en" class="bg-light">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+    <title>Atlética Binária</title>
+</head>
+<body>
+    <header class="container-xxl p-0">
+        <div class="w-100 position-relative">
+            <img class="banner-imagem" src="img/banner3.jpg" alt="Banner com imagem do time da Atlética Binária">
+            <a href="index.php">
+                <picture>
+                    <source srcset="img/logo.jpg" media="(min-width: 600px)">
+                    <img class="logo-image position-absolute top-100 start-50 translate-middle" src="img/logo-menor.jpg" alt="Logo da Atlética">
+                </picture>
+            </a>
+        </div>
+        <div class="container-xxl bg-black banner-faixa p-0 position-relative faixa"></div>
+        <nav class="navbar navbar-expand-lg navbar-dark" >
+                <div class="container-fluid">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <div class="">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Times</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Lojas</a>
+                                </li>
+                            </div>
+                            <div class="">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Eventos</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Galeria</a>
+                                </li>
+                            </div>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+    </header>
+    <main class="container-xxl mt-5">
+
+    </main>
+</body>
+</html>
